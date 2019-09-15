b81d70515954e0bfa351020deccd6cfb39475d95
mati-dominguez <55364003+mati-dominguez@users.noreply.github.com>
2019-09-15 18:59:18 -0300

    fixing css

--- wp-login.php
+++ wp-login.php
@@ -9,11 +9,11 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Acceder &lsaquo; Redux &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='/https://mati-dominguez.github.io/redux/wp-includes/css/dashicons.min.css?ver=5.2.3' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='/https://mati-dominguez.github.io/redux/wp-includes/css/buttons.min.css?ver=5.2.3' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='/https://mati-dominguez.github.io/redux/wp-admin/css/forms.min.css?ver=5.2.3' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='/https://mati-dominguez.github.io/redux/wp-admin/css/l10n.min.css?ver=5.2.3' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='/https://mati-dominguez.github.io/redux/wp-admin/css/login.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='http://localhost/https:/mati-dominguez.github.io/redux/wp-includes/css/dashicons.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='http://localhost/https:/mati-dominguez.github.io/redux/wp-includes/css/buttons.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='http://localhost/https:/mati-dominguez.github.io/redux/wp-admin/css/forms.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='http://localhost/https:/mati-dominguez.github.io/redux/wp-admin/css/l10n.min.css?ver=5.2.3' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='http://localhost/https:/mati-dominguez.github.io/redux/wp-admin/css/login.min.css?ver=5.2.3' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
@@ -22,7 +22,7 @@
 		<div id="login">
 		<h1><a href="https://es.wordpress.org/">Funciona gracias a WordPress</a></h1>
 	
-	<form name="loginform" id="loginform" action="/https://mati-dominguez.github.io/redux/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="http://localhost/https:/mati-dominguez.github.io/redux/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Nombre de usuario o dirección de correo<br />
 		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
@@ -40,7 +40,7 @@
 	</form>
 
 			<p id="nav">
-					<a href="/https://mati-dominguez.github.io/redux/wp-login.php?action=lostpassword">¿Olvidaste tu contraseña?</a>
+					<a href="http://localhost/https:/mati-dominguez.github.io/redux/wp-login.php?action=lostpassword">¿Olvidaste tu contraseña?</a>
 				</p>
 	
 	<script type="text/javascript">
@@ -57,7 +57,7 @@
 			if(typeof wpOnload=='function')wpOnload();
 			</script>
 
-			<p id="backtoblog"><a href="/https://mati-dominguez.github.io/redux/">
+			<p id="backtoblog"><a href="http://localhost/https:/mati-dominguez.github.io/redux/">
 		&larr; Volver a Redux	</a></p>
 			
 	</div>
